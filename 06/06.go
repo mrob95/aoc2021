ba6dc1d214dd9c3c1d569168dd2cb757e7f643d0
Mike Roberts <42875462+mrob95@users.noreply.github.com>
2021-12-06 20:30:04 +0000

    Cleanup

--- 06/06.go
+++ 06/06.go
@@ -9,10 +9,8 @@ import (
 )
 
 func main() {
-	fish := make(map[int]int)
-	for i := 0; i < 9; i++ {
-		fish[i] = 0
-	}
+	var fish [9]int
+
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
 	line := scanner.Text()
@@ -24,16 +22,12 @@ func main() {
 
 	days := 256
 	for day := 0; day < days; day++ {
-		previous := make(map[int]int)
-		for i := 0; i < 9; i++ {
-			previous[i] = fish[i]
-		}
-
+		spawns := fish[0]
 		for i := 0; i < 8; i++ {
-			fish[i] = previous[i+1]
+			fish[i] = fish[i+1]
 		}
-		fish[8] = previous[0]
-		fish[6] += previous[0]
+		fish[8] = spawns
+		fish[6] += spawns
 	}
 
 	total := 0
